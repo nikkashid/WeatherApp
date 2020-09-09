181f0a3e7e7581c8e551c4fefd5dc88575cf026e
nikkashid <nikhil.kashid1@gmail.com>
2020-09-10 02:55:03 +0530

    Creating basic skeleton of network request , using hilt DI for network module and weather repository.

--- app/src/main/java/com/nikhil/weatherapp/application/AppApplication.kt
+++ app/src/main/java/com/nikhil/weatherapp/application/AppApplication.kt
@@ -1,7 +1,6 @@
 package com.nikhil.weatherapp.application
 
 import android.app.Application
-import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
--- app/src/main/java/com/nikhil/weatherapp/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/nikhil/weatherapp/viewModel/HomeViewModel.kt
@@ -7,7 +7,7 @@ import com.nikhil.weatherapp.repositories.WeatherRepository
 class HomeViewModel @ViewModelInject constructor(private val weatherRepository: WeatherRepository) :
     ViewModel() {
 
-    fun getWeather() : Any? {
+    fun getWeather(): Any? {
         return weatherRepository.getCityWeatherFromNetwork("Kolhapur")
     }
 }
